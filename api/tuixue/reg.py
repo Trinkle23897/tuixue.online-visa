21fe6d259e959d7cd9f39c23d18471513306a398
z3dd1cu5 <z3dd1cu5@protonmail.com>
2020-08-04 12:48:05 +0800

    add new places: sg, au

--- api/tuixue/reg.py
+++ api/tuixue/reg.py
@@ -31,7 +31,7 @@ def get_date(page):
 
 def login(cracker, place, requests):
     proxies = g.value("proxies", None)
-    ref = {"北京": "China", "上海": "China", "广州": "China", "成都": "China", "沈阳": "China", "香港": "Hong%20Kong", "台北": "Taiwan", "金边": "Cambodia"}
+    ref = {"北京": "China", "上海": "China", "广州": "China", "成都": "China", "沈阳": "China", "香港": "Hong%20Kong", "台北": "Taiwan", "金边": "Cambodia", "新加坡": "Singapore", "墨尔本": "Australia", "珀斯": "Australia", "悉尼": "Australia"}
 
     # get register page
     REG_URI = "https://cgifederal.secure.force.com/SiteRegister?country=%s&language=zh_CN" % ref[place]
@@ -77,9 +77,9 @@ def login(cracker, place, requests):
         raw = soup.find_all(id='Registration:SiteTemplate:theForm:theId')
         raw = raw[0].attrs['src'].replace('data:image;base64,', '')
         img = base64.b64decode(raw)
-        gifname = 'try.gif'
-        open(gifname, 'wb').write(img)
-        open('gifname', 'w').write(gifname)
+        #gifname = 'try.gif'
+        #open(gifname, 'wb').write(img)
+        #open('gifname', 'w').write(gifname)
         captcha = cracker.solve(img)
         if len(captcha) == 0:
             open('state', 'w').write(
@@ -110,12 +110,12 @@ def login(cracker, place, requests):
             return None
         front_door_uri = r.text.split("'")[-2]
         if front_door_uri.startswith("https"):
-            os.system('mv %s log/%s.gif' % (gifname, captcha))
+            #os.system('mv %s log/%s.gif' % (gifname, captcha))
             break
         else:
-            if not os.path.exists('fail'):
-                os.makedirs('fail')
-            os.system('mv %s fail/%s.gif' % (gifname, captcha))
+            #if not os.path.exists('fail'):
+            #    os.makedirs('fail')
+            #os.system('mv %s fail/%s.gif' % (gifname, captcha))
             if hasattr(cracker, 'wrong'):
                 cracker.wrong()
 
@@ -153,7 +153,11 @@ def visa_select(visa_type, place, sid, requests):
         return None
 
     # select place
-    if place != "香港" and place != "台北" and place != "金边":
+    place2id = {
+        "北京": "0", "成都": "1", "广州": "2", "上海": "3", "沈阳": "4",
+        "墨尔本": "0", "珀斯": "1", "悉尼": "2"
+    }
+    if sum([place == x for x in place2id.keys()]) > 0:
         select_post_uri = "https://cgifederal.secure.force.com/selectpost"
         r = requests.get(select_post_uri, cookies=cookies, proxies=proxies)
         if r.status_code != 200:
@@ -164,14 +168,8 @@ def visa_select(visa_type, place, sid, requests):
         view_state_mac = soup.find(id="com.salesforce.visualforce.ViewStateMAC").get("value")
         view_state_csrf = soup.find(id="com.salesforce.visualforce.ViewStateCSRF").get("value")
         contact_id = soup.find(id="j_id0:SiteTemplate:j_id112:contactId").get("value")
-        place2id = {
-            "北京": "j_id0:SiteTemplate:j_id112:j_id165:0", 
-            "成都": "j_id0:SiteTemplate:j_id112:j_id165:1", 
-            "广州": "j_id0:SiteTemplate:j_id112:j_id165:2", 
-            "上海": "j_id0:SiteTemplate:j_id112:j_id165:3", 
-            "沈阳": "j_id0:SiteTemplate:j_id112:j_id165:4"
-        }
-        place_code = soup.find(id=place2id[place]).get("value")
+        target_id = "j_id0:SiteTemplate:j_id112:j_id165:" + place2id[place]
+        place_code = soup.find(id=target_id).get("value")
         data = {
             "j_id0:SiteTemplate:j_id112": "j_id0:SiteTemplate:j_id112",
             "j_id0:SiteTemplate:j_id112:j_id165": place_code,
@@ -199,11 +197,11 @@ def visa_select(visa_type, place, sid, requests):
     contact_id = soup.find(id="j_id0:SiteTemplate:j_id109:contactId").get("value")
     prefix = "j_id0:SiteTemplate:j_id109:j_id162:"
     category2id = {
-        "B": {"北京": 0, "成都": 0, "广州": 0, "上海": 0, "沈阳": 0, "香港": 1, "台北": 1, "金边": 0},
-        "F": {"北京": 1, "成都": 1, "广州": 1, "上海": 1, "沈阳": 1, "香港": 0, "台北": 0, "金边": 1},
-        "O": {"北京": 4, "成都": 2, "广州": 3, "上海": 4, "沈阳": 2, "香港": 3, "台北": 3, "金边": 2},
-        "H": {"北京": 2, "广州": 3, "上海": 2, "香港": 3, "台北": 3, "金边": 2},
-        "L": {"北京": 3, "广州": 2, "上海": 3, "香港": 3, "台北": 3, "金边": 2}
+        "B": {"北京": 0, "成都": 0, "广州": 0, "上海": 0, "沈阳": 0, "香港": 1, "台北": 1, "金边": 0, "新加坡": 1, "墨尔本": 0, "珀斯": 0, "悉尼": 3},
+        "F": {"北京": 1, "成都": 1, "广州": 1, "上海": 1, "沈阳": 1, "香港": 0, "台北": 0, "金边": 1, "新加坡": 4, "墨尔本": 1, "珀斯": 0, "悉尼": 0},
+        "O": {"北京": 4, "成都": 2, "广州": 3, "上海": 4, "沈阳": 2, "香港": 3, "台北": 3, "金边": 2, "新加坡": 3, "墨尔本": 2, "珀斯": 0, "悉尼": 3},
+        "H": {"北京": 2, "广州": 3, "上海": 2, "香港": 3, "台北": 3, "金边": 2, "新加坡": 3, "墨尔本": 2, "珀斯": 0, "悉尼": 3},
+        "L": {"北京": 3, "广州": 2, "上海": 3, "香港": 3, "台北": 3, "金边": 2, "新加坡": 3, "墨尔本": 2, "珀斯": 0, "悉尼": 3}
     }
     category_code = soup.find(id=prefix + str(category2id[visa_type][place])).get("value")
     data = {
@@ -231,15 +229,15 @@ def visa_select(visa_type, place, sid, requests):
     view_state_mac = soup.find(id="com.salesforce.visualforce.ViewStateMAC").get("value")
     view_state_csrf = soup.find(id="com.salesforce.visualforce.ViewStateCSRF").get("value")
     type2id = {
-        "F": 0, 
-        "B": 2, 
-        "H": 0, 
-        "O": 10 if place == "香港" or place == "台北" or place == "金边" else (6 if place == "广州" else 0),
-        "L": 7 if place == "香港" or place == "台北" or place == "金边" else 1
+        "B": {"北京": 2, "成都": 2, "广州": 2, "上海": 2, "沈阳": 2, "香港": 2, "台北": 2, "金边": 2, "新加坡": 0, "墨尔本": 2, "珀斯": 2, "悉尼": 2},
+        "F": {"北京": 0, "成都": 0, "广州": 0, "上海": 0, "沈阳": 0, "香港": 0, "台北": 0, "金边": 0, "新加坡": 0, "墨尔本": 0, "珀斯": 3, "悉尼": 0},
+        "O": {"北京": 0, "成都": 0, "广州": 6, "上海": 0, "沈阳": 0, "香港": 10, "台北": 10, "金边": 10, "新加坡": 8, "墨尔本": 7, "珀斯": 14, "悉尼": 10},
+        "H": {"北京": 0, "广州": 0, "上海": 0, "香港": 0, "台北": 0, "金边": 0, "新加坡": 0, "墨尔本": 0, "珀斯": 7, "悉尼": 3},
+        "L": {"北京": 1, "广州": 1, "上海": 1, "香港": 7, "台北": 7, "金边": 7, "新加坡": 6, "墨尔本": 5, "珀斯": 12, "悉尼": 8}
     }
     inputs = soup.find_all("input")
     type_codes = [x.get("value") for x in inputs if x.get("name") == "selectedVisaClass"]
-    type_code = type_codes[type2id[visa_type]]
+    type_code = type_codes[type2id[visa_type][place]]
     data = {
         "j_id0:SiteTemplate:theForm": "j_id0:SiteTemplate:theForm",
         "j_id0:SiteTemplate:theForm:j_id178": "继续",
