81c3a5ada91975185e8b0a5288199eb5e3037be4
Benjamin Cai <benjamincaiyh@gmail.com>
2021-01-20 17:01:15 -0600

    frontend: remove debug hook in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Provider } from "react-redux";
 import ReactGA from "react-ga";
 import store from "./redux";
@@ -7,18 +7,11 @@ import { VisaStatusNotification } from "./components";
 import { useInitialDataFetch, useScript } from "./hooks";
 import "./assets/styles/index.less";
 
-let RENDER_TIME = 0; // for debug purpose delete before production
-
 function App() {
     ReactGA.initialize("UA-102409527-2");
     ReactGA.pageview(window.location.pathname + window.location.search);
     useInitialDataFetch();
     useScript("https://busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js");
-
-    useEffect(() => {
-        RENDER_TIME += 1;
-        console.log(`Render the whole page ${RENDER_TIME}`);
-    });
 
     return (
         <>
