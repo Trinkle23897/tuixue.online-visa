efe7e05bd8f00237d9f8479f4bb1b163a71c6710
Trinkle23897 <trinkle23897@gmail.com>
2021-02-21 18:10:44 +0800

    frontend: add swRegistration for compatibility

--- frontend/src/components/VisaStatusNotification.js
+++ frontend/src/components/VisaStatusNotification.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Notification from "react-web-notification";
 import { useWebSocketSubscribe } from "../hooks";
 
 /** Use websocket hook in this empty JSX component instead of global App scope */
 export default function VisaStatusNotification() {
     const [notificationTitle, notificationOption] = useWebSocketSubscribe();
-    return <Notification title={notificationTitle} options={notificationOption} timeout={10000} />;
+    const [swReg, setSwReg] = useState(null);
+    useEffect(() => {
+        navigator.serviceWorker.register("/sw.js").then(reg => {
+            if ("showNotification" in reg) {
+                setSwReg(reg);
+            }
+        });
+    });
+    return (
+        <Notification title={notificationTitle} options={notificationOption} timeout={10000} swRegistration={swReg} />
+    );
 }
